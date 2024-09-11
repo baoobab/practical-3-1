9e3f5bd4682500649182d108afcd0557b4d4d508
baoobab <v.xxl.90@mail.ru>
2024-09-11 22:07:14 +0300

    fix: flushMemory func

--- src/array.cpp
+++ src/array.cpp
@@ -16,16 +16,26 @@ TArray::~TArray() {
 
 void TArray::flushMemory() {
     cout << "Ya crestyanin\n";
-    for (number* curr = this->arr; curr != (this->arr + this->size); curr++) {
-        cout << "flushed: " << *curr << "\n";
-        delete curr;
-    }
+
+
+    // int test = 0;
+    // for (int testCnt = 0; testCnt < this->size; testCnt++) {
+    //     number* curr = (this->arr+testCnt);
+    //     number* prevCurr = (curr - 1);
+    //     if (test++ > 0) {
+    //         cout << "flushed: " << *prevCurr << " " << *curr << "\n";
+    //         delete prevCurr;
+    //     }
+    //     cout << "after: " << *curr << " " << curr << "\n";
+    // }
+    delete[] this->arr; // TODO: podumat'
     this->size = 0;
     this->arr = 0;
 }
 
 void TArray::appendElement(number el) {
     number* newArr = new number[this->size + 1];
+    // number* newArr = (number*) malloc(sizeof(number) * (this->size + 1));
     number cnt = 0; // TODO: mb number zalupa nuzen unsigned hzhzhzh
     for (number* curr = this->arr; curr != (this->arr + this->size); curr++) {
         *(newArr + cnt++) = *curr;
@@ -38,6 +48,7 @@ void TArray::appendElement(number el) {
 }
 
 void TArray::print() {
+    cout << "Size: " << this->size << "\n";
     for (number* curr = this->arr; curr != (this->arr + this->size); curr++) {
         cout << "Elem: " << *curr << "\n";
     }
